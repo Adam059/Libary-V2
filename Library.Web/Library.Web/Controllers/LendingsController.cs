1f779703783ed26db75fb08b31956089a7e5be7d
Adam Mazur <adam.mazur.100@gmail.com>
2019-01-17 00:57:54 +0100

    Add lendings controller

---
+++ Library.Web/Library.Web/Controllers/LendingsController.cs
@@ -0,0 +1,20 @@
+﻿using Library.Web.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Web.Controllers
+{
+    public class LendingsController : Controller
+    {
+        private readonly LibraryContext _context;
+        public LendingsController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
